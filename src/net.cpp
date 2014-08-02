5c0d47b5faa0b1630fb84218f9c8ff0280ff3c60
Sam <salmon92@icloud.com>
2014-08-02 20:58:31 +0100

    Initial commit of Batchcoin.

--- src/net.cpp
+++ src/net.cpp
@@ -1006,6 +1006,7 @@ void ThreadMapPort(void* parg)
         ThreadMapPort2(parg);
         vnThreadsRunning[THREAD_UPNP]--;
     }
+    }
     catch (std::exception& e) {
         vnThreadsRunning[THREAD_UPNP]--;
         PrintException(&e, "ThreadMapPort()");
@@ -1061,7 +1062,7 @@ void ThreadMapPort2(void* parg)
             }
         }
 
-        string strDesc = "SmallChange " + FormatFullVersion();
+        string strDesc = "BitchCoin " + FormatFullVersion();
 #ifndef UPNPDISCOVER_SUCCESS
         /* miniupnpc 1.5 */
         r = UPNP_AddPortMapping(urls.controlURL, data.first.servicetype,
@@ -1727,7 +1728,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. SmallChange is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. BitchCoin is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("BCH");
+    case mBTC: return QString("mBCH");
+    case uBTC: return QString::fromUtf8("μBCH");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("BitchCoin");
+    case mBTC: return QString("Milli-BitchCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-BitchCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
